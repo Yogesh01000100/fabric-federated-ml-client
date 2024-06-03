f605c91f423f85f88958b98d3cba7c55c7ed1fd7
D.Yogesh <yogeshone678@gmail.com>
2024-06-03 14:18:37 +0530

    feat: add fabric analytics client
    Signed-off-by: D.Yogesh <yogeshone678@gmail.com>

---
+++ fabric-samples/backend/backend-service/controllers/hospitalController.js
@@ -0,0 +1,363 @@
+import { gateways } from "../network/fabricNetwork.js";
+import { create } from "kubo-rpc-client";
+import { spawn } from "child_process";
+import axios from "axios";
+import fs from 'fs';
+import os from 'os';
+import csv from 'csv-parser';
+import csvWriter from "csv-write-stream";
+
+
+const homeDirectory = os.homedir();
+const client = create();
+
+function pythonFunction(jsonData) {
+  return new Promise((resolve, reject) => {
+    const pythonProcess = spawn("python", [
+      `${homeDirectory}/fabric-intrachain-client-network-analytics/fabric-samples/backend/backend-service/controllers/python_service.py`,
+    ]);
+
+    pythonProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+      reject(data.toString());
+    });
+
+    pythonProcess.on('close', (code) => {
+      if (code !== 0) {
+        console.log(`Python script exited with code ${code}`);
+        reject(`Python script exited with code ${code}`);
+      } else {
+        const filePath = `${homeDirectory}/fabric-intrachain-client-network-analytics/fabric-samples/backend/backend-service/encrypted_data.dat`;
+        resolve(filePath);
+      }
+    });
+    pythonProcess.stdin.write(jsonData);
+    pythonProcess.stdin.end();
+  });
+}
+
+export const uploadEHR = async (req, res) => {
+  try {
+      const channelName = "mychannel";
+      const chaincodeName = "basic";
+      const filePath = `${homeDirectory}/fabric-intrachain-client-network-analytics/fabric-samples/backend/backend-service/controllers/dataset.csv`;
+      const gateway = gateways["admin"];
+
+      const network = await gateway.getNetwork(channelName);
+      const contract = network.getContract(chaincodeName);
+
+      const results = [];
+
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', async () => {
+          const updatePromises = results.map(async (record, index) => {
+            const patientId = `patient_${index + 1}`;
+            const fileBuffer = Buffer.from(JSON.stringify(record));
+
+            // Add to IPFS
+            const { cid } = await client.add(fileBuffer);
+            console.log(`Data for ${patientId} added to IPFS with CID: ${cid}`);
+
+            // Prepare the data to store in the blockchain
+            const patientData = {
+                disease: {
+                    diabetes: cid.toString()  // Adding the disease tag with IPFS CID
+                }
+            };
+
+            // Store the structured data in the blockchain
+            await contract.submitTransaction('UploadEHR', patientId, JSON.stringify(patientData));
+
+            return { patientId, cid: cid.toString() };
+          });
+
+          const updates = await Promise.all(updatePromises);
+          res.json({
+            success: true,
+            updates: updates,
+            message: "All data uploaded successfully!"
+          });
+        });
+  } catch (error) {
+      console.error(`Error: ${error}`);
+      res.status(500).json({ error: error.message });
+  }
+};
+
+
+export const FetchAllRecords = async (req, res) => {
+  const channelName = "mychannel";
+  const chaincodeName = "basic";
+  const gateway = gateways["admin"]; //  admin access
+
+  try {
+    const network = await gateway.getNetwork(channelName);
+    const contract = network.getContract(chaincodeName);
+    const result = await contract.evaluateTransaction("FetchAllRecords");
+    const resultsJson = JSON.parse(result.toString());
+
+    const fetchedRecords = [];
+
+    for (const record of resultsJson) {
+      const cid = record.disease.diabetes; // Correct path according to the new schema
+      const fileContent = await fetchContentFromIPFS(cid); // fetchContentFromIPFS needs to be defined
+      fetchedRecords.push(JSON.parse(fileContent.toString()));
+    }
+
+    const fields = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age', 'Outcome']; // Modify based on actual fields
+    const csvFilePath = './retrieved_data_set.csv';
+    const writer = csvWriter({ headers: fields });
+    writer.pipe(fs.createWriteStream(csvFilePath));
+
+    fetchedRecords.forEach(record => {
+      writer.write(record);
+    });
+
+    writer.end();
+
+    res.json({
+      success: true,
+      message: "All diabetes records retrieved successfully, with detailed data from IPFS!",
+      filePath: csvFilePath
+    });
+  } catch ( error) {
+    console.error(`Failed to retrieve diabetes records: ${error}`);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+async function fetchContentFromIPFS(cid) {
+  const data = [];
+  for await (const chunk of client.cat(cid)) {
+    data.push(chunk);
+  }
+  return Buffer.concat(data);
+}
+
+
+export const getHospitalRole10 = async (req, res) => {
+  const channelName = "mychannel";
+  const chaincodeName = "basic";
+  const userId = req.query.userId;
+  const keychainrefId = req.query.keychainrefId;
+  //console.log("gateways in controller: ", fabricNetwork.gateways);
+  const gateway = fabricNetwork.gateways[userId];
+  //console.log("user gateway->", gateway);
+  if (!userId) {
+    return res
+      .status(400)
+      .json({ error: "User ID must be provided as a query parameter" });
+  }
+
+  const user = await User.findOne({ u_id: userId });
+
+  if (!gateway || !user) {
+    //console.log(gateways[userId]);
+    return res
+      .status(400)
+      .json({ error: "Error at gateway! Invalid or unspecified user ID" });
+  }
+
+  try {
+    // if keychain id is same then proceed with these if not matching or it has a different keychain_id more than 1 then forward the req
+    // to the cactus server-> if else condition
+    //const apiUrl = `http://localhost:4100/api/cactus-healthcare-backend/get-hospital-data-hspb?keychainrefId=${keychainrefId}`;
+    //const response = await axios.get(apiUrl);
+    //const responseData = response.data;
+    //console.log("data received! : ", responseData, "count : ", (count += 1));
+
+    const network = await gateway.getNetwork(channelName);
+    const contract = network.getContract(chaincodeName);
+    const result = await contract.evaluateTransaction("commonFunction1");
+    const resultJson = JSON.parse(result.toString());
+    console.log("data : ", resultJson, "count: ", (count += 1));
+    res.json({
+      success: true,
+      message: "Hospital data retrieved successfully!",
+      data: responseData,
+    });
+  } catch (error) {
+    console.log(`Failed to retrieve data for user ${userId}: ${error}`);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getHospitalRole20 = async (req, res) => {
+  const channelName = "mychannel";
+  const chaincodeName = "basic";
+  const userId = req.query.userId;
+  //console.log("gateways in controller: ", fabricNetwork.gateways);
+  const gateway = fabricNetwork.gateways[userId];
+  //console.log("user gateway->", gateway);
+  if (!userId) {
+    return res
+      .status(400)
+      .json({ error: "User ID must be provided as a query parameter" });
+  }
+
+  const user = await User.findOne({ u_id: userId });
+
+  if (!gateway || !user) {
+    //console.log(gateways[userId]);
+    return res
+      .status(400)
+      .json({ error: "Error at gateway! Invalid or unspecified user ID" });
+  }
+
+  try {
+    // if keychain id is same then proceed with these if not matching or it has a different keychain_id more than 1 then forward the req
+    // to the cactus server-> if else condition
+    const apiUrl = `http://localhost:4100/api/cactus-healthcare-backend/get-my-profile-patient-hspb?user_id=${userId}`;
+    const response = await axios.get(apiUrl);
+    const responseData = response.data;
+    console.log("data received! : ", responseData);
+
+    /*const network = await gateway.getNetwork(channelName);
+    const contract = network.getContract(chaincodeName);
+    const result = await contract.evaluateTransaction('commonFunction1');
+    const resultJson = JSON.parse(result.toString());
+    console.log('data : ', resultJson, 'count: ', (count += 1));
+    res.json({
+      success: true,
+      message: 'Hospital data retrieved successfully!',
+      data: resultJson,
+    }); */
+  } catch (error) {
+    console.log(`Failed to retrieve data for user ${userId}: ${error}`);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getHospitalRole30 = async (req, res) => {
+  const channelName = "mychannel";
+  const chaincodeName = "basic";
+  const userId = req.query.userId;
+  //console.log("gateways in controller: ", fabricNetwork.gateways);
+  const gateway = fabricNetwork.gateways[userId];
+  //console.log("user gateway->", gateway);
+  if (!userId) {
+    return res
+      .status(400)
+      .json({ error: "User ID must be provided as a query parameter" });
+  }
+
+  const user = await User.findOne({ u_id: userId });
+
+  if (!gateway || !user) {
+    //console.log(gateways[userId]);
+    return res
+      .status(400)
+      .json({ error: "Error at gateway! Invalid or unspecified user ID" });
+  }
+
+  try {
+    // if keychain id is same then proceed with these if not matching or it has a different keychain_id more than 1 then forward the req
+    // to the cactus server-> if else condition
+    const apiUrl = `http://localhost:4100/api/cactus-healthcare-backend/get-my-profile-patient-hspb?user_id=${userId}`;
+    const response = await axios.get(apiUrl);
+    const responseData = response.data;
+    console.log("data received! : ", responseData);
+
+    /*const network = await gateway.getNetwork(channelName);
+    const contract = network.getContract(chaincodeName);
+    const result = await contract.evaluateTransaction('commonFunction1');
+    const resultJson = JSON.parse(result.toString());
+    console.log('data : ', resultJson, 'count: ', (count += 1));
+    res.json({
+      success: true,
+      message: 'Hospital data retrieved successfully!',
+      data: resultJson,
+    }); */
+  } catch (error) {
+    console.log(`Failed to retrieve data for user ${userId}: ${error}`);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getHospitalRole40 = async (req, res) => {
+  const channelName = "mychannel";
+  const chaincodeName = "basic";
+  const userId = req.query.userId;
+  //console.log("gateways in controller: ", fabricNetwork.gateways);
+  const gateway = fabricNetwork.gateways[userId];
+  //console.log("user gateway->", gateway);
+  if (!userId) {
+    return res
+      .status(400)
+      .json({ error: "User ID must be provided as a query parameter" });
+  }
+
+  const user = await User.findOne({ u_id: userId });
+
+  if (!gateway || !user) {
+    // console.log(gateways[userId]);
+    return res
+      .status(400)
+      .json({ error: "Error at gateway! Invalid or unspecified user ID" });
+  }
+
+  try {
+    // if keychain id is same then proceed with these if not matching or it has a different keychain_id more than 1 then forward the req
+    // to the cactus server-> if else condition
+    const apiUrl = `http://localhost:4100/api/cactus-healthcare-backend/get-my-profile-patient-hspb?user_id=${userId}`;
+    const response = await axios.get(apiUrl);
+    const responseData = response.data;
+    console.log("data received! : ", responseData);
+
+    /*const network = await gateway.getNetwork(channelName);
+    const contract = network.getContract(chaincodeName);
+    const result = await contract.evaluateTransaction('commonFunction1');
+    const resultJson = JSON.parse(result.toString());
+    console.log('data : ', resultJson, 'count: ', (count += 1));
+    res.json({
+      success: true,
+      message: 'Hospital data retrieved successfully!',
+      data: resultJson,
+    }); */
+  } catch (error) {
+    console.log(`Failed to retrieve data for user ${userId}: ${error}`);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getHospitalRole50 = async (req, res) => {
+  const channelName = "mychannel";
+  const chaincodeName = "basic";
+  const userId = req.query.userId;
+  const gateway = fabricNetwork.gateways[userId];
+  if (!userId) {
+    return res
+      .status(400)
+      .json({ error: "User ID must be provided as a query parameter" });
+  }
+
+  const user = await User.findOne({ u_id: userId });
+
+  if (!gateway || !user) {
+    return res
+      .status(400)
+      .json({ error: "Error at gateway! Invalid or unspecified user ID" });
+  }
+
+  try {
+    const network = await gateway.getNetwork(channelName);
+    const contract = network.getContract(chaincodeName);
+    const result = await contract.evaluateTransaction("commonFunction1");
+    const resultJson = JSON.parse(result.toString());
+    console.log("data : ", resultJson, "count: ", (count += 1));
+    res.json({
+      success: true,
+      message: "Hospital data retrieved successfully!",
+      data: resultJson,
+    });
+  } catch (error) {
+    console.log(`Failed to retrieve data for user ${userId}: ${error}`);
+    res.status(500).json({ error: error.message });
+  }
+};
---
+++ fabric-samples/backend/backend-service/controllers/python_service.py
@@ -0,0 +1,59 @@
+import os
+import flwr as fl
+import tensorflow as tf
+from sklearn import linear_model # for handling missing values
+import h5py as h5 #To save model weights
+from keras.models import Sequential, model_from_json # to create and save model
+from keras.utils import plot_model
+from keras.layers import Dense
+import numpy as np
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+csv_file = './backend/backend-service/retrieved_data_set.csv'
+data = pd.read_csv(csv_file)
+a = data.isnull().sum()
+b = a.sort_values(ascending=False)
+
+scaler = StandardScaler()
+train_data = data.to_numpy()
+train_data[:, :8] = scaler.fit_transform(train_data[:, :8])
+
+X = train_data[:, [0, 1, 5, 7]]
+Y = pd.get_dummies(train_data[:, 8])
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state=10)
+model = Sequential()
+model.add(Dense(12, input_dim=X_train.shape[1], activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+checkpoint_path = "training_1/blk-cp.weights.h5"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,save_weights_only=True,verbose=1)
+
+class CifarClient(fl.client.NumPyClient):
+    def get_parameters(self, config):
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(X_train, Y_train, epochs=1, batch_size=32, steps_per_epoch=3, 
+                  callbacks=[cp_callback])
+        return model.get_weights(), len(X_train), {}
+
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(X_test, Y_test)
+        print("%s: %.2f%%" % (loss, accuracy))
+        return loss, len(X_test), {"accuracy": float(accuracy)}
+    
+fl.client.start_client(server_address="172.16.10.220:8080", client=CifarClient().to_client())
